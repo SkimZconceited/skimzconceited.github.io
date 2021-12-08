575e809a13873b562c017d525bb8692dbc00b0b7
SkimZ Conceited <Stanleykimambo@gmail.com>
2021-12-08 12:00:36 +0300

    Update index.php

--- ussd/index.php
+++ ussd/index.php
@@ -8,7 +8,7 @@ $text        = $_POST["text"];
 if ($text == "") {
     // This is the first request. Note how we start the response with CON
     $response  = "CON What would you want to check \n";
-    $response .= "1. My Account \n";
+    $response .= "1. Welcome \n";
     $response .= "2. My phone number";
 
 } else if ($text == "1") {
